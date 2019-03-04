8dc613e7c30038139193fef34cc1e9a8da60dff0
wo370506875 <madj@madeMacBook-Pro.local>
2019-03-04 10:39:56 +0800

    localhost

--- src/amqp/amqp.js
+++ src/amqp/amqp.js
@@ -9,14 +9,14 @@ const config = {
     },
     development:{
         protocol:"amqp",
-        hostname:"47.92.131.110",
+        hostname:"localhost",
         port:"6752",
         username:"admin",
         password:"admin"
     },
     production:{
         protocol:"amqp",
-        hostname:"47.92.131.110",
+        hostname:"localhost",
         port:"6752",
         username:"admin",
         password:"admin"
