546943dec40bfb06151c8a440101524a6a6ba994
liuxh <liuxh163git>
2019-05-31 14:24:52 +0800

    change redis db

--- src/db/redis.js
+++ src/db/redis.js
@@ -4,6 +4,16 @@ import redis from 'ioredis'
 if (!process.env.NODE_ENV) { throw new Error('NODE_ENV not set') };
 require('dotenv').config();
 
+const PRD_DB = process.env.REDIS_DB_PRD;
+const DEV_DB = process.env.REDIS_DB_DEV;
 
+let rdb = "";
+if("development" == process.env.NODE_ENV){
+    rdb = DEV_DB;
+}else if("production" == process.env.NODE_ENV){
+    rdb = PRD_DB;
+}
 module.exports = 
-    new redis(process.env.REDIS_PORT, process.env.REDIS_HOST);
+    // new redis(process.env.REDIS_PORT, process.env.REDIS_HOST);
+    new redis(process.env.REDIS_PORT, process.env.REDIS_HOST, {"db": rdb});
+    
